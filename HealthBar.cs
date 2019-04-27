a21bf59658b7a42d5501f478fe51cf29cac82d79
andrulonis <andrulonis@spoko.pl>
2019-04-27 14:45:02 +0200

    healthbar

---
+++ HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    public Text Text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text = GetComponent<Text>();
+    }
+        
+    
+    // Update is called once per frame
+    void Update()
+    {
+       Text.text = slider.value.ToString() + "/100";
+    
+        
+    }
+    
+}
