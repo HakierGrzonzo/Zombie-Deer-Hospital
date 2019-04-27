601146a1114917ce736af07cd80fc3267db6e885
HakierGrzonzo <Grzes869@gmail.com>
2019-04-27 13:45:09 +0200

    added movement
    pls work

---
+++ Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public GameObject self;
+    public int speed;
+    public Vector3 position;
+    public Vector3 playerPos;
+    private Vector3 move;
+    private GameObject playerObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObject = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //construct movement vector
+        position = self.transform.position;
+        playerPos = playerObject.transform.position;
+        Debug.Log(playerPos);
+        Vector3 move = playerPos - position;
+        //normalize
+        move = move.normalized*speed;
+        //rotate self
+    }
+}
