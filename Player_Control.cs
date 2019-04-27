385708dc80cbb1ec36abaa2add386da7ad673743
Tifix <wertynius2@gmail.com>
2019-04-27 17:09:58 +0200

    Omnidirectional walk

--- Player_Control.cs
+++ Player_Control.cs
@@ -6,15 +6,21 @@ public class Player_Control : MonoBehaviour
 {
     private Transform playerTransform;
     private float moveSpeed;
-    private float lastTimeShot;
     public int CameraHeight;
+    public Sprite U;
+    public Sprite UL;
+    public Sprite L;
+    public Sprite DL;
+    public Sprite D;
+    public Sprite DR;
+    public Sprite R;
+    public Sprite UR;
 
     // Start is called before the first frame update
     void Start()
     {
         playerTransform = GetComponent<Transform>();
         moveSpeed = this.gameObject.GetComponent<Mob>().speed;
-        lastTimeShot = Time.time;
     }
 
     // Update is called once per frame
@@ -24,44 +30,43 @@ public class Player_Control : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         Move(vertical, horizontal);
         CameraFollowing();
-        if (Input.GetMouseButtonDown(0) & canShoot())
-        {
-            lastTimeShot = Time.time;
-            this.gameObject.GetComponent<Mob>().Inventory[0].Shoot();
-        }
-    }
-
-    //returns true if the weapon can shoot
-    private bool canShoot()
-    {
-        if (Time.time - lastTimeShot > this.gameObject.GetComponent<Mob>().Inventory[0].fireRate)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        SpriteUpdate();
     }
 
     private void Move(float Vertical, float Horizontal)
     {
         Vector3 currentPos = playerTransform.position;
         Vector3 movementVector = new Vector3(Horizontal,Vertical,0.0f);
-        Debug.Log(movementVector);
-        Vector3 mousePos = Input.mousePosition;
-        Vector3 objectPos = Camera.main.WorldToScreenPoint(playerTransform.position);
-        mousePos.x = mousePos.x - objectPos.x;
-        mousePos.y = mousePos.y - objectPos.y;
-        float playerRotationAngle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-        Quaternion playerRotation =Quaternion.Euler(new Vector3(0, 0, playerRotationAngle));
+        //float playerRotationAngle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        //Quaternion playerRotation =Quaternion.Euler(new Vector3(0, 0, playerRotationAngle));
 
-        playerTransform.SetPositionAndRotation(currentPos+ movementVector*moveSpeed, playerRotation);
+        playerTransform.SetPositionAndRotation(currentPos+ movementVector*moveSpeed, Quaternion.identity);
     }
 
     private void CameraFollowing()
     {
         Camera.main.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -1* CameraHeight);
     }
+    private void SpriteUpdate()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(playerTransform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+        float playerRotationAngle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
 
+        Debug.Log(playerRotationAngle);
+        if (playerRotationAngle      < -157.5 || playerRotationAngle  > 157.5) { gameObject.GetComponent<SpriteRenderer>().sprite = L; Debug.Log("L"); }
+        else if (playerRotationAngle > -22.5  &&  playerRotationAngle < 22.5)  { gameObject.GetComponent<SpriteRenderer>().sprite = R; Debug.Log("R"); }
+        else if(playerRotationAngle  <  -67.5 &&  playerRotationAngle > -112.5) { gameObject.GetComponent<SpriteRenderer>().sprite = U; Debug.Log("U"); }
+        else if(playerRotationAngle  >   67.5 &&  playerRotationAngle < 112.5) { gameObject.GetComponent<SpriteRenderer>().sprite = D; Debug.Log("D"); }
+
+        else if(playerRotationAngle  >   22.5 &&  playerRotationAngle < 67.5) { gameObject.GetComponent<SpriteRenderer>().sprite = DL; Debug.Log("DL"); }
+        else if(playerRotationAngle  <   -22.5 &&  playerRotationAngle > -67.5) { gameObject.GetComponent<SpriteRenderer>().sprite = UL; Debug.Log("UL"); }
+
+        else if(playerRotationAngle  <   157.5  &&  playerRotationAngle > 112.5) { gameObject.GetComponent<SpriteRenderer>().sprite = DR; Debug.Log("DR"); }
+        else if(playerRotationAngle  >   -157.5  &&  playerRotationAngle < -112.5) { gameObject.GetComponent<SpriteRenderer>().sprite = UR; Debug.Log("UR"); }
+
+
+    }
 }
