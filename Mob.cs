f10b8385312ba68dd9d7a77387079d576e402075
Eroar <erykksc@gmail.com>
2019-04-27 14:55:27 +0200

    Added bodyDamage to mobs

--- Mob.cs
+++ Mob.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 
 public class Mob : MonoBehaviour { 
 
+    public int bodyDamage;
     private int HP;
-    private int MaxHP;
-    private float toughness; // it is a multiplier of a damage 
-    private const int InventorySize=5;
+    public int MaxHP;
+    public float toughness; // it is a multiplier of a damage 
+    public const int InventorySize=5;
     public string startingWeaponStr;
     public int speed;
     private Weapons_Module.Weapon[] Inventory;
 
     public Mob(float toughness,int InventorySize, int MaxHP = 100, Weapons_Module.Weapon StartingItem = null)
     {
-        
         this.HP = MaxHP;
         this.MaxHP = MaxHP;
         this.toughness = toughness;
@@ -37,6 +37,20 @@ public class Mob : MonoBehaviour {
             GameObject.Destroy(this.gameObject);
             return true;
         }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (this.gameObject.CompareTag("Player")){
+            if (collision.collider.CompareTag("Enemy"))
+            {
+                Debug.Log("Collision");
+                this.gameObject.GetComponent<Mob>().hit_received(bodyDamage);
+                GameObject.Destroy(collision.collider.gameObject)
+            }
+        }
+        
     }
 
 }
--- bulletScript.cs
+++ bulletScript.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class bulletScript : MonoBehaviour
 {
+    public int bulletDamage;
     void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.CompareTag("Player") || collision.collider.CompareTag("Enemy"))
         {
             Debug.Log("Hit");
-            //collision.collider.gameObject.GetComponent<Mob>().hit_received();
+            collision.collider.gameObject.GetComponent<Mob>().hit_received(bulletDamage);
         }
     }
 }
