1c26eb187d2e90d314c0214fa4e6a4e41087dff2
Eroar <erykksc@gmail.com>
2019-04-27 13:19:24 +0200

    Added Weapon class and Weapons dictionary
    -fixed Mob inventory
    -added Weapon class
    -added Weapons_dict

--- Mob.cs
+++ Mob.cs
@@ -2,39 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Mob : MonoBehaviour { 
 
     public int HP;
     private int MaxHP;
     public const int InventorySize=5;
-    public Item[] Inventory = new Item[InventorySize];
-    
-    /*
-    public bool UseItem(Item UsedItem)
-    {
-        return false;
-        //Use selected item from the inventory, returned value shows if the item was used
-    }
-    */
-    /*
-    public Mob(int MaxHP=100, Item StartingItem=null)
+    public Weapons_Module.Weapon[] Inventory = new Weapons_Module.Weapon[InventorySize];
+
+    public Mob(int MaxHP=100, Weapons_Module.Weapon StartingItem =null)
     {
         this.MaxHP = MaxHP;
         this.Inventory[0] = StartingItem;
         this.HP = MaxHP;
     }
-    */
-    /*
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-*/
+    
 }
--- Weapons_module.cs
+++ Weapons_module.cs
@@ -3,34 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapons_Module : MonoBehaviour {
+    public GameObject normalBulletPrefab;
+    public GameObject waterBulletPrefab;
+    public GameObject 
 
+    void Start()
+    {
+        var Weapons_dict = new Dictionary<string, Weapons_Module.Weapon>()
+            {
+                {"Knife", new Weapon(GameObject bulletPrefeb, GameObject bulletSource, int damage, int fireRate, int bulletSpeed, float bulletFlightTime, int spread, bool isShotgun, bool isMelee)},
+                {"Scythe", new Weapon(GameObject bulletPrefeb, GameObject bulletSource, int damage, int fireRate, int bulletSpeed, float bulletFlightTime, int spread, bool isShotgun, bool isMelee)},
+                {"Water_Gun", new Weapon(GameObject bulletPrefeb, GameObject bulletSource, int damage, int fireRate, int bulletSpeed, float bulletFlightTime, int spread, bool isShotgun, bool isMelee)},
+                {"Machine_Gun", new Weapon(GameObject bulletPrefeb, GameObject bulletSource, int damage, int fireRate, int bulletSpeed, float bulletFlightTime, int spread, bool isShotgun, bool isMelee)},
+                { "Pistol", new Weapon(GameObject bulletPrefeb, GameObject bulletSource, int damage, int fireRate, int bulletSpeed, float bulletFlightTime, int spread, bool isShotgun, bool isMelee)},
+                {"Shotgun",new Weapon(GameObject bulletPrefeb, GameObject bulletSource, int damage, int fireRate, int bulletSpeed, float bulletFlightTime, int spread, bool isShotgun, bool isMelee) }
+            };
+    }
     public class Weapon
     {
-        GameObject bulletPrefab;
+        GameObject bulletPrefeb;
         GameObject bulletSource;
         int damage;
         int fireRate;
         int bulletSpeed;
-        float bulletDistance;
+        float bulletFlightTime;
         int spread;
-
-        public Weapon(GameObject bulletPrefab, GameObject bulletSource, int damage, int fireRate, int bulletSpeed, float bulletDistance, int spread)
+        bool isShotgun;
+        bool isMelee;
+        
+        
+        public Weapon(GameObject bulletPrefeb, GameObject bulletSource, int damage, int fireRate, int bulletSpeed, float bulletFlightTime, int spread, bool isShotgun, bool isMelee)
         {
-            this.bulletPrefab = bulletPrefab;
+            this.bulletPrefeb = bulletPrefeb;
             this.bulletSource = bulletSource;
             this.damage = damage;
             this.fireRate = fireRate;
             this.bulletSpeed = bulletSpeed;
-            this.bulletDistance = bulletDistance;
+            this.bulletFlightTime = bulletFlightTime;
             this.spread = spread;
+            this.isShotgun = isShotgun;
+            this.isMelee = isMelee;
         }
-
+        
         void Shoot()
         {
 
             Transform bulletSourceTransform = bulletSource.GetComponent<Transform>();
-            GameObject Bullet = Instantiate(bulletPrefab, bulletSourceTransform.position, bulletSourceTransform.rotation);
+            GameObject Bullet = Instantiate(bulletPrefeb, bulletSourceTransform.position, bulletSourceTransform.rotation);
             Bullet.GetComponent<Rigidbody>().AddForce(Bullet.transform.right * bulletSpeed);
+            GameObject.Destroy(Bullet,bulletFlightTime);
+            //Albo moze zadzialac Destroy(Bullet,DeathTime), sprawdzic jak nie zadziala pierwsze
+
         }
+
     }
 }
