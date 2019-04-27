db9b8dd99dc1016e8fd7426cb3c95e4bdd98cafa
Tifix <wertynius2@gmail.com>
2019-04-27 15:33:46 +0200

    CameraFollowing

--- Player_Control.cs
+++ Player_Control.cs
@@ -6,6 +6,7 @@ public class Player_Control : MonoBehaviour
 {
     private Transform playerTransform;
     private float moveSpeed;
+    public int CameraHeight;
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +21,14 @@ public class Player_Control : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
         Move(vertical, horizontal);
-        
+        CameraFollowing();
     }
 
     private void Move(float Vertical, float Horizontal)
     {
         Vector3 currentPos = playerTransform.position;
         Vector3 movementVector = new Vector3(Horizontal,Vertical,0.0f);
-
+        Debug.Log(movementVector);
         Vector3 mousePos = Input.mousePosition;
         Vector3 objectPos = Camera.main.WorldToScreenPoint(playerTransform.position);
         mousePos.x = mousePos.x - objectPos.x;
@@ -36,6 +37,11 @@ public class Player_Control : MonoBehaviour
         Quaternion playerRotation =Quaternion.Euler(new Vector3(0, 0, playerRotationAngle));
 
         playerTransform.SetPositionAndRotation(currentPos+ movementVector*moveSpeed, playerRotation);
+    }
+
+    private void CameraFollowing()
+    {
+        Camera.main.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -1* CameraHeight);
     }
 
 }
