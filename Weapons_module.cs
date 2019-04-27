b2c170764ce9f0f9792139733ca5fc0717d1caa0
Eroar <erykksc@gmail.com>
2019-04-27 08:43:57 +0200

    Added a weapon constructor to Weapons_module

---
+++ Weapons_module.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons_Module : MonoBehaviour {
+
+    public class Weapon
+    {
+        GameObject bulletPrefab;
+        GameObject bulletSource;
+        int damage;
+        int fireRate;
+        int bulletSpeed;
+        float bulletDistance;
+        int spread;
+
+        public Weapon(GameObject bulletPrefab, GameObject bulletSource, int damage, int fireRate, int bulletSpeed, float bulletDistance, int spread)
+        {
+            this.bulletPrefab = bulletPrefab;
+            this.bulletSource = bulletSource;
+            this.damage = damage;
+            this.fireRate = fireRate;
+            this.bulletSpeed = bulletSpeed;
+            this.bulletDistance = bulletDistance;
+            this.spread = spread;
+        }
+
+        void Shoot()
+        {
+
+            Transform bulletSourceTransform = bulletSource.GetComponent<Transform>();
+            GameObject Bullet = Instantiate(bulletPrefab, bulletSourceTransform.position, bulletSourceTransform.rotation);
+            Bullet.GetComponent<Rigidbody>().AddForce(Bullet.transform.right * bulletSpeed);
+        }
+    }
+}
