94235d7ac074d549f6420c526350c004abb84083
Tifix <wertynius2@gmail.com>
2019-04-27 11:02:31 +0200

    Shooting that works.
    Add this and Weapon items to player for shooty dooty

--- Shoot.cs
+++ Shoot.cs
@@ -2,34 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapons_Module : MonoBehaviour {
+public class Shoot : MonoBehaviour {
 
-    public class Weapon
+    private float LocalFireRate;
+    private float LocalSpread;
+    private float FireRateCtrl;
+    private bool IsShotgun;
+    public GameObject BulletSource;
+    public Vector3 FirDir;
+
+
+	// Use this for initialization
+	void Start ()
     {
+        //FireRateCtrl = 0;
 
+    }
+	
 
-        public GameObject bulletPrefab;
-        public GameObject bulletSource;
-        public int damage;
-        public int fireRate;
-        public int bulletSpeed;
-        public float bulletDistance;
-        public int spread;
-        public float timer;
-
-        void Shoot()
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
         {
-
-            Transform bulletSourceTransform = bulletSource.GetComponent<Transform>();
-            GameObject Bullet = Instantiate(bulletPrefab, bulletSourceTransform.position, bulletSourceTransform.rotation);
-            Bullet.GetComponent<Rigidbody>().AddForce(Bullet.transform.right * bulletSpeed);
-            function OnCollisionEnter(){Destroy(Bullet};
-            timer += 1.0f * Time.deltaTime;
-            if (timer >=5)
+            foreach (Item itchild in gameObject.GetComponentsInChildren<Item>())
             {
-                GameObject.Destroy(Bullet);
-             }
-            //Albo moze zadzialac Destroy(Bullet,DeathTime), sprawdzic jak nie zadziala pierwsze
+                if (itchild.CurrentlyEquipped == true)
+                {
+                    LocalFireRate = itchild.FireRate;
+                    LocalSpread = itchild.Spread;
+                    IsShotgun = itchild.IsShotgun;
+                }
+            }
+            if (Time.time> FireRateCtrl + LocalFireRate)
+            {
+                FirDir = (transform.right * 500);
+                GameObject Bullet = Instantiate(BulletSource, transform.position, Quaternion.identity);
+                Bullet.GetComponent<Rigidbody>().AddForce(FirDir);                     
+                if (IsShotgun)
+                {
+                    GameObject Bullet2 = Instantiate(BulletSource, transform.position + transform.forward * 10, Quaternion.identity);
+                    GameObject Bullet3 = Instantiate(BulletSource, transform.position + transform.forward * 5, Quaternion.identity);
+                    GameObject Bullet4 = Instantiate(BulletSource, transform.position + transform.forward * -5, Quaternion.identity);
+                    GameObject Bullet5 = Instantiate(BulletSource, transform.position + transform.forward * -10, Quaternion.identity);
+
+
+                    FirDir = RotateAroundAxis(FirDir, LocalSpread, transform.up);
+                    Bullet2.GetComponent<Rigidbody>().AddForce(FirDir);
+                    FirDir = RotateAroundAxis(FirDir, LocalSpread, transform.up);
+                    Bullet3.GetComponent<Rigidbody>().AddForce(FirDir);
+                    FirDir = RotateAroundAxis(FirDir, -3* LocalSpread, transform.up);
+                    Bullet4.GetComponent<Rigidbody>().AddForce(FirDir);
+                    FirDir = RotateAroundAxis(FirDir, -1* LocalSpread, transform.up);
+                    Bullet5.GetComponent<Rigidbody>().AddForce(FirDir);                   
+                }
+                FireRateCtrl = Time.time;
+
+            }
+
+
+
+            
         }
     }
+    public static Vector3 RotateAroundAxis(Vector3 v, float a, Vector3 axis)
+    {
+        var q = Quaternion.AngleAxis(a, axis);
+        return q * v;
+    }
+
 }
