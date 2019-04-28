1a82c276766439ef5e1626ae01a2f7ba0326b84d
Eroar <erykksc@gmail.com>
2019-04-28 16:51:35 +0200

    Update bulletScript.cs

--- bulletScript.cs
+++ bulletScript.cs
@@ -12,6 +12,10 @@ public class bulletScript : MonoBehaviour
             Debug.Log("Hit");
             collider.gameObject.GetComponent<Mob>().hit_received(bulletDamage);
         }
+        else if (collider.CompareTag("Player"))
+        {
+            
+        }
         else
         {
             GameObject.Destroy(this.gameObject);
