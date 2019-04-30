b79b264bbe8bb39b98e005d70b2fc220da8bbf34
andrulonis <andrulonis@spoko.pl>
2019-04-30 03:41:23 +0200

    bul
    sadff

--- Weapons_module.cs
+++ Weapons_module.cs
@@ -198,7 +198,7 @@ public class Weapons_Module : MonoBehaviour{
             */
             case "Flame_Thrower":
 
-                Weapons_Module.Weapon Flame_Thrower  = new Weapon(weaponName,3, 2, 8, bulletSource, fireBulletPrefab, 7, 0.05f, speedMult * 7.5f, 1.9f, 10,true, Resources.Load < AudioClip >("SoundEffects/sdFlamethrower"));
+                Weapons_Module.Weapon Flame_Thrower  = new Weapon(weaponName,3, 2, 8, bulletSource, bfire, 7, 0.05f, speedMult * 7.5f, 1.9f, 10,true, Resources.Load < AudioClip >("SoundEffects/sdFlamethrower"));
 
                 return (Flame_Thrower);
             /*
