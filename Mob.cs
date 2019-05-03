b5a92a34057e111cfe1ebbe66a4808a9f0ac9ddc
Eroar <erykksc@gmail.com>
2019-05-03 22:38:09 +0200

    added tag to dropped pickup

--- Mob.cs
+++ Mob.cs
@@ -111,6 +111,7 @@ public class Mob : MonoBehaviour {
             GameObject droppedWeapon = Instantiate(Weapons_Module.droppedWeaponPrefab, gameObject.transform.position, Quaternion.identity);
             droppedWeapon.GetComponent<SpriteRenderer>().sprite = Weapons_Module.GetWeaponSprite(Inventory[inventorySlot].name);
             droppedWeapon.GetComponent<WeaponPickup>().weaponToDropName = Inventory[inventorySlot].name;
+            droppedWeapon.tag = "Pickup";
         }
     }
 
